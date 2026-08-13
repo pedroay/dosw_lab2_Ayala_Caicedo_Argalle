5ce05f9531c023a4ceaa200bba7a4e44b45004e1
BySam00 <samuel.argalle@mail.escuelaing.edu.co>
2026-08-13 11:06:56 -0500

    Creacion de pom.xml, application.java

---
+++ src/main/java/edu/eci/dosw/Application.java
@@ -0,0 +1,7 @@
+package edu.eci.dosw;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Maven project configured and running correctly.");
+    }
+}
