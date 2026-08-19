447826548532544ff666b1c49cf2e667e795b86d
unknown <ayalapedro5e@gmail.com>
2026-08-19 11:08:04 -0500

    i ends some classes

---
+++ src/main/java/edu/eci/dosw/reto3/AirVehicule.java
@@ -0,0 +1,12 @@
+package edu.eci.dosw.reto3;
+
+public interface AirVehicule {
+
+    public String getType();
+
+    public String getCategory();
+    double getMaximumSpeed();
+    double getPrice();
+    String getComfort();
+    String getEquipment();
+}
